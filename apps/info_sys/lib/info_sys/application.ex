3ee4fbb17189bb7ecda71dd70393c99ad20be541
Shawn Vo <shawn.vo@gmail.com>
2019-06-01 20:32:57 -0400

    added info sys

---
+++ apps/info_sys/lib/info_sys/application.ex
@@ -0,0 +1,21 @@
+defmodule InfoSys.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: InfoSys.Worker.start_link(arg)
+      # {InfoSys.Worker, arg}
+      InfoSys.Supervisor
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one ]
+    Supervisor.start_link(children, opts)
+  end
+end
---
+++ config/dev.secret.exs
@@ -0,0 +1,2 @@
+use Mix.Config
+config :rumbl, :wolfram, app_id: "W623KT-7JH664WRAU"
